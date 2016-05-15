aff2d24d2d3619da40e7f7d744cb361b9cefdf04
Christian Ulrich <info@cu-tec.de>
2016-05-15 18:24:12 +0200

    Runtime only package

--- packages/lazarus/zcomponent_nogui.pas
+++ packages/lazarus/zcomponent_nogui.pas
@@ -2,20 +2,13 @@
   This source is only used to compile and install the package.
  }
 
-unit zcomponent_nogui;
+unit zcomponent_nogui; 
 
 interface
 
 uses
-  ZConnection, ZDatasetUtils, ZSqlMetadata, ZDataset, ZSqlMonitor, 
-  LazarusPackageIntf;
+  ZConnection, ZDatasetUtils, ZSqlMetadata, ZDataset, ZSqlMonitor;
 
 implementation
 
-procedure Register;
-begin
-end;
-
-initialization
-  RegisterPackage('zcomponent_nogui', @Register);
 end.
